d1edcbd91a5c8fdbd0f43ed208485705ece95613
refirst11 <ocelot2828@gmail.com>
2025-05-17 17:16:42 +0900

    fix: add strings arguments

--- src/utils/container.ts
+++ src/utils/container.ts
@@ -1,12 +1,17 @@
 type RangeContainerQuery<S extends string> = `@container (${S})`;
 type ContainerQueryType = 'max-width' | 'min-width' | 'max-height' | 'min-height';
-type ContainerQuery<S extends ContainerQueryType, N extends number> = `@container (${S}: ${N}px)`;
+type ContainerQuery<S extends ContainerQueryType, V extends string | number> = V extends number ? `@container (${S}: ${V}px)` : `@container (${S}: ${V})`;
+
+const createContainerQuery = <S extends ContainerQueryType>(queryType: S) => {
+  return <V extends string | number>(value: V): ContainerQuery<S, V> =>
+    (typeof value === 'number' ? `@container (${queryType}: ${value}px)` : `@container (${queryType}: ${value})`) as ContainerQuery<S, V>;
+};
 
 const range = <S extends string>(range: S): RangeContainerQuery<S> => `@container (${range})`;
-const maxWidth = <N extends number>(num: N): ContainerQuery<'max-width', N> => `@container (max-width: ${num}px)`;
-const maxHeight = <N extends number>(num: N): ContainerQuery<'max-height', N> => `@container (max-height: ${num}px)`;
-const minWidth = <N extends number>(num: N): ContainerQuery<'min-width', N> => `@container (min-width: ${num}px)`;
-const minHeight = <N extends number>(num: N): ContainerQuery<'min-height', N> => `@container (min-height: ${num}px)`;
+const maxWidth = createContainerQuery('max-width');
+const maxHeight = createContainerQuery('max-height');
+const minWidth = createContainerQuery('min-width');
+const minHeight = createContainerQuery('min-height');
 
 export const container = {
   range,
--- src/utils/media.ts
+++ src/utils/media.ts
@@ -1,14 +1,19 @@
 type RangeMediaQuery<S extends string> = `@media (${S})`;
 type MediaQueryType = 'max-width' | 'min-width' | 'max-height' | 'min-height';
-type MediaQuery<S extends MediaQueryType, N extends number> = `@media (${S}: ${N}px)`;
+type MediaQuery<S extends MediaQueryType, N extends string | number> = `@media (${S}: ${N}px)`;
+
+const createMediaQuery = <S extends MediaQueryType>(queryType: S) => {
+  return <V extends string | number>(value: V): MediaQuery<S, V> =>
+    (typeof value === 'number' ? `@media (${queryType}: ${value}px)` : `@media (${queryType}: ${value})`) as MediaQuery<S, V>;
+};
 
 const dark = '@media (prefers-color-scheme: dark)' as const;
 const light = '@media (prefers-color-scheme: light)' as const;
 const range = <S extends string>(range: S): RangeMediaQuery<S> => `@media (${range})`;
-const maxWidth = <N extends number>(num: N): MediaQuery<'max-width', N> => `@media (max-width: ${num}px)`;
-const maxHeight = <N extends number>(num: N): MediaQuery<'max-height', N> => `@media (max-height: ${num}px)`;
-const minWidth = <N extends number>(num: N): MediaQuery<'min-width', N> => `@media (min-width: ${num}px)`;
-const minHeight = <N extends number>(num: N): MediaQuery<'min-height', N> => `@media (min-height: ${num}px)`;
+const maxWidth = createMediaQuery('max-width');
+const maxHeight = createMediaQuery('max-height');
+const minWidth = createMediaQuery('min-width');
+const minHeight = createMediaQuery('min-height');
 
 export const media = {
   dark,
