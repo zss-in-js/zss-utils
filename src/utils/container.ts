68f24b38626bbd90a91bc7d7f0f59e96cabd722f
refirst11 <ocelot2828@gmail.com>
2025-05-17 12:51:22 +0900

    feat: update container and media utility functions for type safety

--- src/utils/container.ts
+++ src/utils/container.ts
@@ -1,9 +1,17 @@
-const range = (range: string) => `@container (${range})` as '@container ()';
-const max = (str: string) => `@container (max-${str})` as '@container (max-)';
-const min = (str: string) => `@container (min-${str})` as '@container (min-)';
+type RangeContainerQuery<S extends string> = `@container (${S})`;
+type ContainerQueryType = 'max-width' | 'min-width' | 'max-height' | 'min-height';
+type ContainerQuery<S extends ContainerQueryType, N extends number> = `@container (${S}: ${N}px)`;
+
+const range = <S extends string>(range: S): RangeContainerQuery<S> => `@container (${range})`;
+const maxWidth = <N extends number>(num: N): ContainerQuery<'max-width', N> => `@container (max-width: ${num}px)`;
+const maxHeight = <N extends number>(num: N): ContainerQuery<'max-height', N> => `@container (max-height: ${num}px)`;
+const minWidth = <N extends number>(num: N): ContainerQuery<'min-width', N> => `@container (min-width: ${num}px)`;
+const minHeight = <N extends number>(num: N): ContainerQuery<'min-height', N> => `@container (min-height: ${num}px)`;
 
 export const container = {
   range,
-  max,
-  min,
+  maxWidth,
+  maxHeight,
+  minWidth,
+  minHeight,
 };
--- src/utils/media.ts
+++ src/utils/media.ts
@@ -1,15 +1,21 @@
-export const scheme = {
-  dark: '@media (prefers-color-scheme: dark)',
-  light: '@media (prefers-color-scheme: light)',
-} as const;
+type RangeMediaQuery<S extends string> = `@media (${S})`;
+type MediaQueryType = 'max-width' | 'min-width' | 'max-height' | 'min-height';
+type MediaQuery<S extends MediaQueryType, N extends number> = `@media (${S}: ${N}px)`;
 
-const range = (range: string) => `@media (${range})` as '@media ()';
-const max = (str: string) => `@media (max-${str})` as '@media (max-)';
-const min = (str: string) => `@media (min-${str})` as '@media (min-)';
+const dark = '@media (prefers-color-scheme: dark)' as const;
+const light = '@media (prefers-color-scheme: light)' as const;
+const range = <S extends string>(range: S): RangeMediaQuery<S> => `@media (${range})`;
+const maxWidth = <N extends number>(num: N): MediaQuery<'max-width', N> => `@media (max-width: ${num}px)`;
+const maxHeight = <N extends number>(num: N): MediaQuery<'max-height', N> => `@media (max-height: ${num}px)`;
+const minWidth = <N extends number>(num: N): MediaQuery<'min-width', N> => `@media (min-width: ${num}px)`;
+const minHeight = <N extends number>(num: N): MediaQuery<'min-height', N> => `@media (min-height: ${num}px)`;
 
 export const media = {
+  dark,
+  light,
   range,
-  max,
-  min,
-  scheme,
+  maxWidth,
+  maxHeight,
+  minWidth,
+  minHeight,
 };
