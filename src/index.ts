92a57fe0c7402aee8b61e4fb07ed644c69b8a22e
refirst11 <ocelot2828@gmail.com>
2025-08-15 04:52:19 +0900

    fix: ps path

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { media } from './utils/media';
 export { container } from './utils/container';
 export { color } from './utils/color';
-export { ps } from './utils/pseudo';
+export { ps } from './utils/ps';
 export { px } from './utils/px';
